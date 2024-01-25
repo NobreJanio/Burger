7426a2ffd7f4afb24879bc84917839ee2ac4cec4
NobreJanio <janionobrega@hotmail.com>
2024-01-25 17:01:57 -0300

    Add Nodemon, Sucrase, Eslint, Prettier, MVC, Docker fist container, SQL/ Sequelize, fist Migration, create & configure Model

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import { v4 } from 'uuid'
+import User from './app/models/User'
+
+const routes = new Router()
+
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Ana Maria',
+    email: 'anamaria@email.com',
+    password_hash: '5454533',
+  })
+  return response.json(user)
+})
+
+export default routes
