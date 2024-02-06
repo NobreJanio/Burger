f9fbda928f1efa1d7182e755f3d41916967c3f6c
NobreJanio <janionobrega@hotmail.com>
2024-02-05 21:08:03 -0300

    create o controller of products

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
+import ProductController from './app/controllers/ProductController'
 
 // para  criar as rotas, é necessário instanciar o router e utilizá-lo como parâmetro na criação das rotas.
 const routes = new Router()
@@ -9,5 +10,7 @@ const routes = new Router()
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
+
+routes.post('/products', ProductController.store)
 
 export default routes
